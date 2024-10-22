0ccaec144a62a6c595296caaab4b76ba8182014b
Kay-Robert Dormann <kay-robert.dormann@pkm.tu-darmstadt.de>
2024-10-22 18:33:12 +0200

    fixed add_particle test

--- test/test_trajectory.py
+++ test/test_trajectory.py
@@ -118,6 +118,7 @@ class TestParticleTrajectory(unittest.TestCase):
 
     def test_ptype_type(self):
         self.traj.add_particle_info(1, "key", "value")
+        self.assertRaises(TypeError, self.traj.add_particle_info, "asdf")
         self.traj.add_particle_info("asdf", "key2", "value2")
         self.assertTrue(
             isinstance(self.traj.get_particle_info(1), dict),
