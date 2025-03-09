d36c40cd2206281f2f96a0433bcb2c2ede7c4967
hechtprojects <lukas.hecht@pkm.tu-darmstadt.de>
2025-03-09 16:22:43 +0100

    copyright info updated in all files

--- amep/cluster.py
+++ amep/cluster.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # =============================================================================
-# Copyright (C) 2023-2024 Lukas Hecht and the AMEP development team.
+# Copyright (C) 2023-2025 Lukas Hecht and the AMEP development team.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
