8b125eb63d9ec1aa230e1d143148646f8314dbe0
Kay-Robert Dormann <86723788+kay-ro@users.noreply.github.com>
2025-03-09 15:56:49 +0100

    updated utils.domain_length description

--- amep/utils.py
+++ amep/utils.py
@@ -1301,7 +1301,8 @@ def domain_length(
 
         L(t) = 2\pi\frac{\int_{q_{\rm min}}^{q_{\rm max}}{\rm d}q\,S(q,t)}{\int_{q_{\rm min}}^{q_{\rm max}}{\rm d}q\,qS(q,t)}
 
-    and has been used in Refs. [1]_ [2]_ [3]_ [4]_ for example.
+    where :math:`q=n \frac{2\pi}{L}` with :math:`n\in \mathbb{N}` and box legth :math:`L`.
+    It has been used in Refs. [1]_ [2]_ [3]_ [4]_ for example.
 
     References
     ----------
@@ -1344,10 +1345,6 @@ def domain_length(
     l: float
         Domain length as inverse expectation value of q.
 
-    Notes
-    -------
-    Here, qmin is set to the default value 2*np.pi/boxsize. Please check also the 2D isotropic static structure factor (amep.spatialcor.sfiso).
-    
     
     Examples
     --------
